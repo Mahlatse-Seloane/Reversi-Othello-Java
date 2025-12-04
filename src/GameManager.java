a1f7dda5c15015641fd81caa1dd5c93b58cad833
MahlatseSeloane <mahlatseseloane97@gmail.com>
2025-12-04 13:54:10 +0200

    feat(GameManager): update initializeBoard to accept boardSize parameter

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,26 +1,28 @@
 public class GameManager
 {
     private Board board;
-    private int currentBoardSize;
+    private int curBoardSize;
     private int availableSpaces;
+    private SquareState curPToken;
 
     GameManager()
     {
 
     }
 
-    private void initializeBoard()
+    private void initializeBoard(final int boardSize)
     {
-        board = new Board(currentBoardSize);
+        curBoardSize = boardSize;
+        board = new Board(curBoardSize);
 
-        int initialRow = (currentBoardSize/2) - 1;
-        int initialCol = (currentBoardSize/2) - 1;
+        int initialRow = (curBoardSize/2) - 1;
+        int initialCol = (curBoardSize/2) - 1;
 
         board.setCellContent(initialRow, initialCol,SquareState.BLACK);
         board.setCellContent(initialRow, initialCol+1,SquareState.WHITE);
         board.setCellContent(initialRow+1, initialCol,SquareState.WHITE);
         board.setCellContent(initialRow+1, initialCol+1,SquareState.BLACK);
 
-        availableSpaces = (currentBoardSize * currentBoardSize) - 4;
+        availableSpaces = (curBoardSize * curBoardSize) - 4;
     }
 }
