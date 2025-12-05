6b905ae96d3f1c5019c05b07c909c8502dfad21e
MahlatseSeloane <mahlatseseloane97@gmail.com>
2025-12-05 17:16:18 +0200

    feat(PlayerType): add getter/setter methods for player ID and player token

--- src/PlayerType.java
+++ src/PlayerType.java
@@ -1,7 +1,29 @@
+import java.util.ArrayList;
+
 public class PlayerType
 {
-    public PlayerType()
-    {
+    private SquareState token;
+    private String playerID;
 
+    public PlayerType() {}
+
+    public void setPlayerToken(final SquareState token)
+    {
+        this.token = token;
+    }
+
+    public void setPlayerID(final String ID)
+    {
+        this.playerID = ID;
+    }
+
+    public SquareState getPlayerToken()
+    {
+        return token;
+    }
+
+    public String getPlayerID()
+    {
+        return playerID;
     }
 }
