3579db0dbecd43437c4a638f2ac6b5a0d3f5266c
MahlatseSeloane <mahlatseseloane97@gmail.com>
2025-12-08 23:18:52 +0200

    feat(InputValidator): add ReadInt method to validator integer input from the user

--- src/InputValidator.java
+++ src/InputValidator.java
@@ -1,4 +1,44 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class InputValidator
 {
+    private static final Scanner scanner = new Scanner(System.in);
+
     private InputValidator() {}
+
+    public static int ReadInt(final String prompt,final int minRange,final int maxRange)
+    {
+        boolean inputValid = false;
+        int tempInput = 0;
+
+        System.out.println();
+        System.out.print(prompt);
+        do
+        {
+            try
+            {
+                tempInput = scanner.nextInt();
+                if (tempInput >= minRange && tempInput <= maxRange)
+                    inputValid = true;
+                else
+                    System.out.printf("Error: Input out of range. Please enter a number (%d-%d): ", minRange, maxRange);
+            }
+            catch (InputMismatchException e)
+            {
+                System.out.printf("Error: Invalid input. Please enter a whole number (%d - %d): ",minRange,maxRange);
+            }
+            catch (Exception e)
+            {
+                System.out.println("\nSOMETHING WENT WRONG");
+            }
+            finally
+            {
+                scanner.nextLine(); // clear invalid input
+            }
+        }
+        while (!inputValid);
+
+        return tempInput;
+    }
 }
